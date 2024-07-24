827ea3311d5bb8e93d76181212b662634b4dbec4
Mazharrais <mazharmajo72@gmail.com>
2024-07-24 14:13:02 +0500

    add code

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ createBtn.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild(img);
 
 })
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    }
+})
